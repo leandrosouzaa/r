5704e4d65bd7b2536beec79cb062ed822f4678dc
leandrosouzaa <leandror.dev@gmail.com>
2020-10-12 18:32:40 -0300

    Aula 06

---
+++ Curso Zurubabel/6.r
@@ -0,0 +1,15 @@
+# Aula 6 - Conversão (coersão) de dados
+
+# ambiente
+varC <- "5"
+varI <- 5
+varN <- 4.3
+varL <- "TRUE"
+
+# is.integer para inteiro
+# is.character para texto
+# is.logical para booleano
+
+as.logical(varI)
+
+as.character(varL)
