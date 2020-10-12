644a930916b9d6086b73dfd83ca371d4f6e1aa17
leandrosouzaa <leandror.dev@gmail.com>
2020-10-12 14:26:06 -0300

    Aula 01

---
+++ Curso Zurubabel/1.r
@@ -0,0 +1,27 @@
+# Utiliza-se # para fazer comentários.
+
+# As 4 operações básicas
+
+# Adição
+4 + 5
+
+# Subtração
+4 - 5
+
+# Multiplicação
+4 * 5
+
+# Divisão
+4 / 5
+
+# Exponenciação
+4 ^ 5
+
+# Resto de divisão
+4 %% 5
+
+# Ordem de parenteses
+(4 + 4) * 5
+
+# Ordem matemática
+4 + 4 * 5
