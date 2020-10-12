fab523c2dd73cb9e991b44b5977150e354815df2
leandrosouzaa <leandror.dev@gmail.com>
2020-10-12 18:32:08 -0300

    Aula 03

---
+++ Curso Zurubabel/3.r
@@ -0,0 +1,19 @@
+# Aula 3 - Workspace (Básico)
+
+# Criar variável x
+
+x <- 0
+
+# Criando Outra variável
+variavelWorkSpace <- "exemplo"
+
+# Listar os objetos no workspace
+ls()
+
+# Apagar objeto do Workspace
+# rm(nome da variável)
+
+# Apagar Todos rm (list = ls())
+rm(list = ls())
+
+ls()
