375a44293560079d72d3ad61231dd986027c3d93
leandrosouzaa <leandror.dev@gmail.com>
2020-10-12 18:32:19 -0300

    Aula 04

---
+++ Curso Zurubabel/4.R
@@ -0,0 +1,21 @@
+# Aula 4 - Tipos básicos de dados
+
+# (Números)
+# Inteiro - Integer
+4
+class(4)
+# Sempre colocar o L na frente do número para o R entender que é um número inteiro.
+class(4L)
+
+# Ponto Flutuante - Numeric
+4.3
+class(4.3333333)
+
+# Caractere = character
+"Leandro"
+class("Leandro")
+
+# Lógico - Lógico True ou False / T ou F
+class(TRUE) #logical
+class(FALSE) #logical
+class(T)
