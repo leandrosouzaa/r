c65b244e81ef608ca8dec4880685f49d4d5e5a38
leandrosouzaa <leandror.dev@gmail.com>
2020-10-12 18:32:28 -0300

    Aula 05

---
+++ Curso Zurubabel/5.r
@@ -0,0 +1,8 @@
+# Aula 5 - Verificação de Tipo de Dado
+
+# Funções is.(tipo de dado)([variavel a ser verificada]) -> True ou False
+
+# is.numeric - se é ponto flutuante
+# is.integer - se é inteiro
+
+is.integer(5.3)
