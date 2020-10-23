37b3752eef7fec7b8717f48857dbe561446a9431
leandrosouzaa <leandror.dev@gmail.com>
2020-10-23 13:09:55 -0300

    Aula 07

---
+++ Curso Zurubabel/7.r
@@ -0,0 +1,18 @@
+# Aula 7 - Vetores
+
+# Criando Vetores - função C
+
+vitorias <- c(5, 4, 1, 6, 2)
+
+vitorias
+
+# VETOREM NO R COMEÇAM PELO 1
+vitorias[1]
+
+x <- 90
+x[1]
+
+is.vector(x) # True
+is.vector(90) # True
+is.vector(90[1]) #True
+
