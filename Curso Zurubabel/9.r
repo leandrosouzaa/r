9169709509ea06c631ca86edd36cda06c7501094
leandrosouzaa <leandror.dev@gmail.com>
2020-10-23 13:10:25 -0300

    aula 09

---
+++ Curso Zurubabel/9.r
@@ -0,0 +1,24 @@
+# Aula 10 - Operações Matemáticas com Vetores
+
+# Vetor a ser calculado
+x <- c(1:10)
+
+# Soma +10 a cada elemento do vetor
+x + 10 
+
+# Multplica +10 a cada elemento do vetor
+x * 10 
+
+# Multplicação entre vetores
+y <- c(2:11)
+x * y
+
+# vetor menor que o alvo
+y <- c(1:3)
+x * y
+
+# Ajustar
+y <- c(1:2)
+x * y
+
+
