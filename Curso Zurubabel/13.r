e0c7f56e1b733d45adff44c52a989fa7fc5d790e
leandrosouzaa <leandror.dev@gmail.com>
2020-10-23 18:43:04 -0300

    Aula 13

---
+++ Curso Zurubabel/13.r
@@ -0,0 +1,14 @@
+# Aula 14 - rbind e cbind
+
+# Começa a distribuir linha por linha
+rbind(1:4, 2:5)
+
+# Começa coluna por coluna
+cbind(1:4, 2:5)
+
+# Exemplo
+v
+vetorPessoas <- c("Xicão", "Zé", "Tião", "Pedrão")
+vetorCachacasIngeridas <- c(3, 4, 1, 12)
+
+cbind(vetorPessoas, vetorCachacasIngeridas)
