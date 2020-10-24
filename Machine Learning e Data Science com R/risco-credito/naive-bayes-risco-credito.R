823ba7ca33e997e535e9384406fef9d162a15d25
leandrosouzaa <leandror.dev@gmail.com>
2020-10-24 19:19:35 -0300

    Risco Crédito

---
+++ Machine Learning e Data Science com R/risco-credito/naive-bayes-risco-credito.R
@@ -0,0 +1,19 @@
+base = read.csv("www/r/Machine Learning e Data Science com R/risco_credito.csv")
+
+install.packages("e1071")
+library(e1071)
+
+classificador = naiveBayes(x = base[-5], y = base$risco)
+print(classificador)
+
+# historia: boa, divida: alta, garantias: nenhuma, renda: > 35
+# historia: ruim, divida: baixa, garantias: adequada, renda:  < 15
+historia = c("ruim")
+divida = c("alta")
+garantias = c("adequada")
+renda = c("0_15")
+df = data.frame(historia, divida, garantias, renda)
+
+previsao = predict(classificador, newdata = df, 'raw')
+print(previsao)
+
